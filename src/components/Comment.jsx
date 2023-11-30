7423b2a51d405999b266d8d8c281068bdef724c8
Quentin Gans <contact@quentingans.fr>
2023-11-30 12:03:21 +0100

    Add blog api

---
+++ src/components/Comment.jsx
@@ -0,0 +1,9 @@
+const Comment = ({ comment }) => (
+    <li className="list-group-item">
+        <strong>De :</strong> {comment.email}
+        <br />
+        <strong>Contenu : </strong>{comment.body}
+    </li>
+)
+
+export default Comment
